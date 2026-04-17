1bfa9f3ab02b64f3d979fb392111d2c39d22347c
Sergei Skoropupov <donckou@gmail.com>
2026-04-17 15:43:52 +0300

    feat: добавлено перемещение по ^J ^K в code action

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,21 @@ vim.pack.add({
     "https://github.com/nvim-telescope/telescope-ui-select.nvim",
 })
 
+local actions = require("telescope.actions")
+
 require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+            },
+            n = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+            },
+        },
+    },
     extensions = {
         ["ui-select"] = {
             require("telescope.themes").get_dropdown({
